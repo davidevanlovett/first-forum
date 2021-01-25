1fe9a49d20df3b969f995d3b49a8606adb9bd24f
davidevanlovett@gmail.com <davidevanlovett@gmail.com>
2021-01-24 19:36:25 -0700

    initial framework from .net 5.0

---
+++ first-forum/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using first_forum.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace first_forum.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
